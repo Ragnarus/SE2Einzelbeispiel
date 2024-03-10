58816103bd48351f39be3c40d369972069e5e0b5
Alexander Krampl <119439649+Ragnarus@users.noreply.github.com>
2024-03-10 10:34:08 +0100

    Erstellen der onClick Methode für Abschicken
    Es gab hier Probleme mit BufferedWriter deshalb hab ich DataOutputStream genommen.

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.einzelbeispiel;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +29,43 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btnAbschicken = findViewById(R.id.cmdAbschicken);
+        TextView textServerAntwort = findViewById(R.id.txtServerAntwort);
+        EditText textMartikelnummer = findViewById(R.id.editMartikelnummer);
+
+        btnAbschicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textMartikel = String.valueOf(textMartikelnummer.getText());
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Socket socket = new Socket("se2-submission.aau.at",20080);
+
+                            //BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                            //out.write("12103912"+"\n");
+
+                            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                            out.writeBytes(textMartikel + "\n");
+
+                            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            String messageReceived = in.readLine();
+                            socket.close();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textServerAntwort.setText(messageReceived);
+                                }
+                            });
+
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }).start();
+            }
         });
     }
 }
